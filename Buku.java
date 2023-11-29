b1f526d4e49ae56a305a93c4c3943fbba5e8319d
Pradya Hening Pertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2023-11-30 02:09:15 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,35 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pboteori;
-
-import java.util.List;
-
-/**
- *
- * @author PRADYA
- */
-public class Buku {
+class Buku {
+    private int idBuku;
     private String judul;
     private String pengarang;
     private String nomorISBN;
     private boolean statusKetersediaan;
+    private static final int DURASI_MAX_PINJAMAN = 7; // Contoh durasi maksimal 7 hari
+    private static final int DENDA_PER_HARI = 500; // Contoh denda per hari 500
 
-    public Buku(String judul, String pengarang, String nomorISBN, boolean statusKetersediaan) {
+    public Buku(int idBuku, String judul, String pengarang, String nomorISBN) {
+        this.idBuku = idBuku;
         this.judul = judul;
         this.pengarang = pengarang;
         this.nomorISBN = nomorISBN;
-        this.statusKetersediaan = statusKetersediaan;
+        this.statusKetersediaan = true;
     }
 
-    public boolean cek() {
+    public boolean cekKetersediaan() {
         return statusKetersediaan;
     }
 
-    public String displayInfo() {
-        return "\nJudul: " + judul + "\nPengarang: " + pengarang +
-                "\nNomor ISBN: " + nomorISBN + "\nStatus Ketersediaan: " + (statusKetersediaan ? "Tersedia" : "Tidak Tersedia");
+    public String getNomorISBN() {
+        return nomorISBN;
     }
+
+    public void setStatusKetersediaan(boolean statusKetersediaan) {
+        this.statusKetersediaan = statusKetersediaan;
+    }
+    
+    public static int getDurasiMaxPinjaman() {
+        return DURASI_MAX_PINJAMAN;
+    }
+
+    public static int getDendaPerHari() {
+        return DENDA_PER_HARI;
+    }
+
+    public int getIdBuku() {
+        return idBuku;
+    }
+
+    public void setIdBuku(int idBuku) {
+        this.idBuku = idBuku;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+    
+    
 }
