b5fee9e573e9aed025bd51fdc84e8c1ae969df38
Pradya Hening Pertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2023-11-30 02:08:30 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,72 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pboteori;
-
-import java.util.List;
-
-/**
- *
- * @author PRADYA
- */
-import java.util.ArrayList;
-import java.util.List;
-
-public class Admin {
-    private static int counter = 1;
+class Admin {
     private int idAdmin;
-    private String nama;
+    private String namaAdmin;
     private String username;
     private String password;
 
-    public Admin(String nama, String username, String password) {
-        this.idAdmin = counter++;
-        this.nama = nama;
-        this.username = username;
-        this.password = password;
-    }
-
-    public boolean Login(String inputUsername, String inputPassword) {
-        return this.username.equals(inputUsername) && this.password.equals(inputPassword);
-    }
-
-    public void tambahBuku(Buku buku) {
-        List<Buku> daftarBuku = new ArrayList<>();
-        daftarBuku.add(buku);
-    }
-
-    public static void setCounter(int counter) {
-        Admin.counter = counter;
-    }
-
-    public void setIdAdmin(int idAdmin) {
+    public Admin(int idAdmin, String namaAdmin, String username, String password) {
         this.idAdmin = idAdmin;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public void setUsername(String username) {
+        this.namaAdmin = namaAdmin;
         this.username = username;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public static int getCounter() {
-        return counter;
-    }
-
-    public int getIdAdmin() {
-        return idAdmin;
-    }
-
-    public String getNama() {
-        return nama;
     }
 
     public String getUsername() {
@@ -76,7 +18,4 @@ public class Admin {
     public String getPassword() {
         return password;
     }
-    
-    
-    
 }
