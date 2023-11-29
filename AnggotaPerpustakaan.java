76c014323df1e3f061a22ef33c7d519424e27f29
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 08:02:13 +0700

    Create AnggotaPerpustakaan.java

---
+++ AnggotaPerpustakaan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tahap2_pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class AnggotaPerpustakaan {
+    private int idAnggota;
+    private int nomorAnggota;
+    private String nama;
+    private String alamat;
+    private String sejarahPeminjaman;
+
+    public void register() {
+        // Implementasi registrasi anggota
+    }
+
+    public String displayPeminjaman() {
+        // Implementasi tampilan peminjaman
+        return sejarahPeminjaman;
+    }
+}
+
