75c4616060068bc9a558a384717cf79651973afc
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 08:03:36 +0700

    Create Norifikasi.java

---
+++ Norifikasi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tahap2_pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class Notifikasi {
+    private int idNotifikasi;
+    private AnggotaPerpustakaan anggota;
+    private String isi;
+
+    public void kirimNotifikasi() {
+        // Implementasi pengiriman notifikasi
+    }
+
+    public String displayNotifikasi() {
+        // Implementasi tampilan notifikasi
+        return "";
+    }
+}
+
