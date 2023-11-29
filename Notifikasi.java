3c9f87811b18fd24a484b604850896bfb94aecba
Pradya Hening Pertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2023-11-30 02:09:37 +0700

    Update Notifikasi.java

--- Notifikasi.java
+++ Notifikasi.java
@@ -1,32 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pboteori;
-
-/**
- *
- * @author PRADYA
- */
-public class Notifikasi {
-    private static int counter = 1;
+class Notifikasi {
     private int idNotifikasi;
     private AnggotaPerpustakaan anggota;
-    private String isi;
+    private String isiNotifikasi;
 
-    public Notifikasi(AnggotaPerpustakaan anggota, String isi) {
-        this.idNotifikasi = counter++;
+    public Notifikasi(int idNotifikasi, AnggotaPerpustakaan anggota, String isiNotifikasi) {
+        this.idNotifikasi = idNotifikasi;
         this.anggota = anggota;
-        this.isi = isi;
-    }
-
-    public void kirimNotifikasi() {
-        System.out.println("Notifikasi untuk " + anggota.displayPeminjaman() + ": " + isi);
-    }
-
-    public String displayNotifikasi() {
-        return "ID Notifikasi: " + idNotifikasi + "\nAnggota: " + anggota.displayPeminjaman() +
-                "\nIsi: " + isi;
+        this.isiNotifikasi = isiNotifikasi;
     }
 }
