d68308d3c9049747dd7c04c6cd89379c97a0fc80
Pradya Hening Pertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2023-11-30 02:10:11 +0700

    Update TransaksiPengembalian.java

--- TransaksiPengembalian.java
+++ TransaksiPengembalian.java
@@ -1,73 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pboteori;
-
-import java.util.Date;
-
-/**
- *
- * @author PRADYA
- */
-import java.util.Date;
-
-public class TransaksiPengembalian {
-    private static int counter = 1;
+class TransaksiPengembalian {
     private int idTransaksi;
-    private Date tglKembali;
-    private TransaksiPeminjaman transaksiPeminjam;
+    private TransaksiPeminjaman transaksiPeminjaman;
+    private Date tanggalKembali;
 
-    public TransaksiPengembalian(TransaksiPeminjaman transaksiPeminjam) {
-        this.idTransaksi = counter++;
-        this.tglKembali = new Date();
-        this.transaksiPeminjam = transaksiPeminjam;
-    }
-
-    public int Denda() {
-        int dendaPerHari = 1000;
-        long selisihHari = (tglKembali.getTime() - transaksiPeminjam.tanggalPinjam.getTime()) / (24 * 60 * 60 * 1000);
-        int denda = Math.max(0, (int) (selisihHari - transaksiPeminjam.durasiPinjam) * dendaPerHari);
-        return denda;
-    }
-
-    public String displayInfo() {
-        return "ID Transaksi: " + idTransaksi + "\nTanggal Kembali: " + tglKembali +
-                "\nDenda: " + Denda() + "\nTransaksi Peminjam: " + transaksiPeminjam.displayInfo();
-    }
-
-    public static int getCounter() {
-        return counter;
-    }
-
-    public static void setCounter(int counter) {
-        TransaksiPengembalian.counter = counter;
-    }
-
-    public int getIdTransaksi() {
-        return idTransaksi;
-    }
-
-    public void setIdTransaksi(int idTransaksi) {
+    public TransaksiPengembalian(int idTransaksi, TransaksiPeminjaman transaksiPeminjaman, Date tanggalKembali) {
         this.idTransaksi = idTransaksi;
+        this.transaksiPeminjaman = transaksiPeminjaman;
+        this.tanggalKembali = tanggalKembali;
     }
+}
 
-    public Date getTglKembali() {
-        return tglKembali;
-    }
+class Notifikasi {
+    private int idNotifikasi;
+    private AnggotaPerpustakaan anggota;
+    private String isiNotifikasi;
 
-    public void setTglKembali(Date tglKembali) {
-        this.tglKembali = tglKembali;
+    public Notifikasi(int idNotifikasi, AnggotaPerpustakaan anggota, String isiNotifikasi) {
+        this.idNotifikasi = idNotifikasi;
+        this.anggota = anggota;
+        this.isiNotifikasi = isiNotifikasi;
     }
-
-    public TransaksiPeminjaman getTransaksiPeminjam() {
-        return transaksiPeminjam;
-    }
-
-    public void setTransaksiPeminjam(TransaksiPeminjaman transaksiPeminjam) {
-        this.transaksiPeminjam = transaksiPeminjam;
-    }
-    
-    
 }
