21f9d00bf891ba31c0d14e639e4d4dd201a26347
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 08:03:11 +0700

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tahap2_pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class Admin {
+    private int idAdmin;
+    private String nama;
+    private String username;
+    private String password;
+
+    public boolean Login() {
+        // Implementasi proses login admin
+        return false;
+    }
+
+    public void tambahBuku() {
+        // Implementasi penambahan buku oleh admin
+    }
+}
+
