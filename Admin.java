3e8efc7dec03273322d9eaea80ca2dd45084a071
Pradya Hening Pertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2023-11-29 21:52:47 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,33 +3,80 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tahap2_pbo;
+package pboteori;
+
+import java.util.List;
 
 /**
  *
- * @author HP
+ * @author PRADYA
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Admin {
+    private static int counter = 1;
     private int idAdmin;
     private String nama;
     private String username;
     private String password;
 
-    public Admin(int idAdmin, String nama, String username, String password) {
-        this.idAdmin = idAdmin;
+    public Admin(String nama, String username, String password) {
+        this.idAdmin = counter++;
         this.nama = nama;
         this.username = username;
         this.password = password;
     }
 
-    public boolean Login() {
-        return username.equals("admin") && password.equals("admin123");
+    public boolean Login(String inputUsername, String inputPassword) {
+        return this.username.equals(inputUsername) && this.password.equals(inputPassword);
     }
 
     public void tambahBuku(Buku buku) {
-        System.out.println("Buku berhasil ditambahkan: " + buku.getJudul());
+        List<Buku> daftarBuku = new ArrayList<>();
+        daftarBuku.add(buku);
     }
+
+    public static void setCounter(int counter) {
+        Admin.counter = counter;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    
+    
 }
-
-
-
