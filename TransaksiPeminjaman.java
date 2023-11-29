f92df6b3c17f529f4bb8acb8e01cce9459d76efd
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 09:15:34 +0700

    Update TransaksiPeminjaman.java

--- TransaksiPeminjaman.java
+++ TransaksiPeminjaman.java
@@ -18,14 +18,32 @@ public class TransaksiPeminjaman {
     private Date tanggalPinjam;
     private int durasiPinjam;
 
+    public TransaksiPeminjaman(int idTransaksi, AnggotaPerpustakaan anggota, Buku buku, Date tanggalPinjam, int durasiPinjam) {
+        this.idTransaksi = idTransaksi;
+        this.anggota = anggota;
+        this.buku = buku;
+        this.tanggalPinjam = tanggalPinjam;
+        this.durasiPinjam = durasiPinjam;
+    }
+
     public int Denda() {
-        // Implementasi perhitungan denda
-        return 0;
+        int dendaPerHari = 100;
+        Date currentDate = new Date();
+        long selisihHari = (currentDate.getTime() - tanggalPinjam.getTime()) / (24 * 60 * 60 * 1000);
+        int denda = (int) Math.max(0, selisihHari * dendaPerHari);
+        return denda;
     }
 
     public String displayInfo() {
-        // Implementasi tampilan informasi transaksi peminjaman
-        return "";
+        return String.format("ID Transaksi: %d, Buku: %s, Tanggal Pinjam: %s, Durasi Pinjam: %d hari",
+                idTransaksi, buku.getJudul(), tanggalPinjam.toString(), durasiPinjam);
     }
+
+    public Date getTanggalPinjam() {
+        return tanggalPinjam;
+    }
+
 }
+
+
 
