fcecbb82d7f9f300741ef8b14b48c20f2af7ad84
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 08:05:01 +0700

    Create TransaksiPengembalian.java

---
+++ TransaksiPengembalian.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tahap2_pbo;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.Date;
+
+public class TransaksiPengembalian {
+    private int idTransaksi;
+    private Date tglKembali;
+    private TransaksiPeminjaman transaksiPeminjam;
+
+    public int Denda() {
+        // Implementasi perhitungan denda pengembalian
+        return 0;
+    }
+
+    public String displayInfo() {
+        // Implementasi tampilan informasi transaksi pengembalian
+        return "";
+    }
+}
+
