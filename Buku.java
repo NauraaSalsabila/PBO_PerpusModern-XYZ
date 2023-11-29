82a0b36a4ae2f42b29afde8daf7669d69278884a
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 08:02:50 +0700

    Create Buku.java

---
+++ Buku.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tahap2_pbo;
+
+/**
+ *
+ * @author HP
+ */
+public class Buku {
+    private int idBuku;
+    private String judul;
+    private String pengarang;
+    private String nomorISBN;
+    private boolean statusKetersediaan;
+
+    public boolean cek() {
+        // Implementasi pengecekan status ketersediaan buku
+        return false;
+    }
+
+    public String displayInfo() {
+        // Implementasi tampilan informasi buku
+        return "";
+    }
+}
+
