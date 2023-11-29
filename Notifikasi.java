5341412527acbd4c1d4b06d75707579b742530ae
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 09:15:15 +0700

    Update Notifikasi.java

--- Notifikasi.java
+++ Notifikasi.java
@@ -9,40 +9,24 @@ package tahap2_pbo;
  *
  * @author HP
  */
-import java.util.Date;
-
-public class TransaksiPeminjaman {
-    private int idTransaksi;
+public class Notifikasi {
+    private int idNotifikasi;
     private AnggotaPerpustakaan anggota;
-    private Buku buku;
-    private Date tanggalPinjam;
-    private int durasiPinjam;
+    private String isi;
 
-    public TransaksiPeminjaman(int idTransaksi, AnggotaPerpustakaan anggota, Buku buku, Date tanggalPinjam, int durasiPinjam) {
-        this.idTransaksi = idTransaksi;
+    public Notifikasi(int idNotifikasi, AnggotaPerpustakaan anggota, String isi) {
+        this.idNotifikasi = idNotifikasi;
         this.anggota = anggota;
-        this.buku = buku;
-        this.tanggalPinjam = tanggalPinjam;
-        this.durasiPinjam = durasiPinjam;
+        this.isi = isi;
     }
 
-    public int Denda() {
-        int dendaPerHari = 100;
-        Date currentDate = new Date();
-        long selisihHari = (currentDate.getTime() - tanggalPinjam.getTime()) / (24 * 60 * 60 * 1000);
-        int denda = (int) Math.max(0, selisihHari * dendaPerHari);
-        return denda;
+    public void kirimNotifikasi() {
+        System.out.println("Notifikasi terkirim ke: " + anggota.getNama());
     }
 
-    public String displayInfo() {
-        return String.format("ID Transaksi: %d, Buku: %s, Tanggal Pinjam: %s, Durasi Pinjam: %d hari",
-                idTransaksi, buku.getJudul(), tanggalPinjam.toString(), durasiPinjam);
+    public String displayNotifikasi() {
+        return String.format("ID Notifikasi: %d, Isi: %s", idNotifikasi, isi);
     }
-
-    public Date getTanggalPinjam() {
-        return tanggalPinjam;
-    }
-
 }
 
 
