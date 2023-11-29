a37c27860066625a3a55b381a0e7c5aa161a33ad
NauraaSalsabila <112566581+NauraaSalsabila@users.noreply.github.com>
2023-11-29 09:13:02 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -9,19 +9,53 @@ package tahap2_pbo;
  *
  * @author HP
  */
-public class Admin {
-    private int idAdmin;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnggotaPerpustakaan {
+    private int idAnggota;
+    private int nomorAnggota;
     private String nama;
-    private String username;
-    private String password;
+    private String alamat;
+    private List<TransaksiPeminjaman> sejarahPeminjaman;
 
-    public boolean Login() {
-        // Implementasi proses login admin
-        return false;
+    public AnggotaPerpustakaan(int idAnggota, int nomorAnggota, String nama, String alamat) {
+        this.idAnggota = idAnggota;
+        this.nomorAnggota = nomorAnggota;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.sejarahPeminjaman = new ArrayList<>();
     }
 
-    public void tambahBuku() {
-        // Implementasi penambahan buku oleh admin
+    public void register() {
+        System.out.println("Anggota berhasil diregistrasi");
     }
+
+    public String displayPeminjaman() {
+        StringBuilder result = new StringBuilder("Sejarah Peminjaman:\n");
+        for (TransaksiPeminjaman transaksi : sejarahPeminjaman) {
+            result.append(transaksi.displayInfo()).append("\n");
+        }
+        return result.toString();
+    }
+
+    public void tambahPeminjaman(TransaksiPeminjaman transaksi) {
+        sejarahPeminjaman.add(transaksi);
+    }
+
+    public int getIdAnggota() {
+        return idAnggota;
+    }
+
+    public int getNomorAnggota() {
+        return nomorAnggota;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
 }
+
+
 
